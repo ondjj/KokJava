e90d7163236c4bbada19383b32ef857228e7a9ed
ondjj <jcjk0302@gmail.com>
2022-06-24 12:58:50 +0900

    "220624"

--- src/study/prob02/ErrorFix.java
+++ src/study/prob02/ErrorFix.java
@@ -24,6 +24,21 @@ public class ErrorFix {
 		System.out.println("f="+f);
 		System.out.println("l="+l);
 		System.out.println("result="+ result);
+		
+		System.out.println("int 타입의 최대값: " + Integer.MAX_VALUE);
+		System.out.println("int 타입의 최소값: " + Integer.MIN_VALUE);
+		
+		System.out.println("byte 최대값: " + Byte.MAX_VALUE);
+		System.out.println("byte 최소값: " + Byte.MIN_VALUE);
+		
+		System.out.println("short 최대값: " + Short.MAX_VALUE);
+		System.out.println("short 최소값: " + Short.MIN_VALUE);
+		
+		System.out.println("char 최대값: " + (int)Character.MAX_VALUE);
+		System.out.println("char 최소값: " + (int   )Character.MIN_VALUE);
+		
+		System.out.println("long 최대값: " + Long.MAX_VALUE);
+		System.out.println("long 최소값: " + Long.MIN_VALUE);
 
 	}
 
