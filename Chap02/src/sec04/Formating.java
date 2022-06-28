810ef76020130910d83c00fbace92f0c7addf6f3
ondjj <jcjk0302@gmail.com>
2022-06-28 17:50:33 +0900

    "~~"

---
+++ Chap02/src/sec04/Formating.java
@@ -0,0 +1,16 @@
+package sec04;
+
+public class Formating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("전창준");
+		System.out.print("전창준");
+		System.out.print("전창준\n");
+		System.out.printf("이름:%s 나이:%d 취미:%s \n", "전창준",28, "낚시");
+		System.out.printf("이름: %2$s | 주소 : %1$s | 번호 : %s", "전창준", "광주 북구", "010-2251-6108");
+		
+
+	}
+
+}
---
+++ src/study/prob04/MutiplicationTable.java
@@ -0,0 +1,22 @@
+package study.prob04;
+
+public class MutiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=3;j++) {
+				int x = j+1+(i-1)/3*3;
+				int y = i%3==0?3:i%3;
+				
+				if(x>9) // 9단까지만 출력하게 해주는 코드
+					break;
+				System.out.print(x+"*"+y+"="+x*y+"\t");
+			}
+			System.out.println();
+			if(i%3==0) System.out.println();
+		}
+
+	}
+
+}
