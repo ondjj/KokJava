44a6dd5fc6d9cf0fdebf70cca012f9bb63190f83
ondjj <jcjk0302@gmail.com>
2022-07-05 18:01:56 +0900

    "backjun clock wise"

--- src/study/prob05/Main.java
+++ src/study/prob05/Main.java
@@ -7,21 +7,31 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
-		int x = scanner.nextInt();
-		int y = scanner.nextInt();
-		
-		if((x>0)&&(y>0)) {
-			System.out.println(1);
-		} else if((x<0) && (y>0)) {
-			System.out.println(2);
-		
-		} else if((x>0)&&(y<0)) {
-			System.out.println(4);
-		} else {
-			System.out.println(3);
-		}
 
+		int H = scanner.nextInt();
+		int M = scanner.nextInt();
+
+		if (H == 0) {
+			if ((M - 45) < 0) {
+				H += 23;
+				M = 60 + (M - 45);
+				System.out.printf("%d %d",H,M);
+			} else {
+				M = M - 45;
+				System.out.printf("%d %d",H,M);
+			}
+		} else if (H > 0) {
+			if ((M - 45) < 0) {
+				H -= 1;
+				M = 60 + (M - 45);
+				System.out.printf("%d %d",H,M);
+			} else {
+				M = M - 45;
+				
+				System.out.printf("%d %d",H,M);
+			}
+
+		}
 	}
 
 }
