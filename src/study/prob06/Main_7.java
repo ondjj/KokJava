9e83f4dc65053c7651ea677f9b461be50a7c3c5d
ondjj <jcjk0302@gmail.com>
2022-07-11 17:08:02 +0900

    prob fix

--- src/study/prob06/Main_7.java
+++ src/study/prob06/Main_7.java
@@ -7,35 +7,36 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main_7 {
-	
-	public static void main(String[] args)throws IOException {
-		
+
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
-		for(int i=1;i<=N;i++) {
-			for(int j=1;j<=i;j++) {
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
 				bw.write("*");
-			}bw.newLine();
+			}
+			bw.newLine();
 		}
-		
+
 		br.close();
 		bw.flush();
 		bw.close();
+
 		/*
 		 * Scanner scanner = new Scanner(System.in);
 		 * 
 		 * int N = scanner.nextInt();
 		 * 
-		 * for (int i = 1; i <= N; i++) { for(int j=1; j <= i; j++) {
+		 * for (int i = 1; i <= N; i++) { for (int j = 1; j <= i; j++) {
 		 * System.out.print("*"); } System.out.println();
 		 * 
 		 * }
 		 */
-	}
-		
+
 	}
 
-
+}
