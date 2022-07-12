327c1f3f3f20426f94b070ec43b1fb790fac10d8
ondjj <jcjk0302@gmail.com>
2022-07-12 17:50:36 +0900

    backjun

--- src/study/prob07/Main_4.java
+++ src/study/prob07/Main_4.java
@@ -8,38 +8,34 @@ public class Main_4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*
-		 * 문제 
-		 * 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 
-		 * 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다.
-		 * 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
-		 * 입력 
-		 * 첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
+		 * 문제 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는
+		 * 1, 2, 0, 2이다. 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는
+		 * 프로그램을 작성하시오. 입력 첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이
+		 * 아닌 정수이다.
 		 * 
-		 * 출력 
-		 * 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+		 * 출력 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
-		int[] arr = new int[10]; 
+		int[] arr = new int[10];
 		int count = 0;
-		
-		for(int i=0; i<10;i++) {
+
+		for (int i = 0; i < 10; i++) {
 			arr[i] = scanner.nextInt();
-			
-			
-			for(int j=0;j<10;j++) {
+			arr[i] = arr[i] % 42;
+			System.out.println(">>>"+arr[i]);
+		}
+		for (int j = 0; j < 10; j++) {
+			if (arr[j] != arr[j]+1) {
+				System.out.println(">>>>"+arr[j]);
+				count++;
+			}else {
 				
-				arr[i] = arr[i] % 42;
-				if(arr[i] != arr[j]) {
-					
-					count++;
-				}
 			}
 		}
-		scanner.close();
 		System.out.println(count);
+		scanner.close();
 
-		
 	}
 
 }
