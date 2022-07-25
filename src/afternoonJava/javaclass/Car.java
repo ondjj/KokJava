864dd62cdf7358bd8265d01330953b42c091b12e
ondjj <jcjk0302@gmail.com>
2022-07-25 16:45:08 +0900

    --

--- src/afternoonJava/javaclass/Car.java
+++ src/afternoonJava/javaclass/Car.java
@@ -3,6 +3,17 @@ package afternoonJava.javaclass;
 // 클래스 선언
 public class Car {
 	
+	// 필드 선언
+	// String 타입의 company 필드를 선언 ( 초기값 -> 현대자동차)
+	String company = "현대자동차";
+	
+	String model = "그랜저";
+	
+	String color = "검정";
+	
+	int maxSpeed = 350;
+	
+	int speed;
 
 }
 
---
+++ src/afternoonJava/javaclass/CarExample.java
@@ -0,0 +1,47 @@
+package afternoonJava.javaclass;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new Car();
+		
+		System.out.println("제작 회사 : "+ car.company);
+		System.out.println("모델명 : "+ car.model);
+		System.out.println("색깔 : "+ car.color);
+		System.out.println("최고 속도 : "+ car.maxSpeed);
+		System.out.println("현재 속도 : "+ car.speed);
+		
+		System.out.println();
+		
+		car.company = "기아자동차";
+		car.color = "빨강";
+		car.maxSpeed = 380;
+		car.speed = 100;
+		car.model = "카니발";
+		
+		System.out.println("제작 회사 : "+ car.company);
+		System.out.println("모델명 : "+ car.model);
+		System.out.println("색깔 : "+ car.color);
+		System.out.println("최고 속도 : "+ car.maxSpeed);
+		System.out.println("현재 속도 : "+ car.speed);
+		
+		System.out.println();
+		
+		Car kia = new Car();
+		kia.company = "기아자동차";
+		kia.color = "빨강";
+		kia.maxSpeed = 380;
+		kia.speed = 100;
+		kia.model = "카니발";
+		
+		System.out.println("제작 회사 : "+ kia.company);
+		System.out.println("모델명 : "+ kia.model);
+		System.out.println("색깔 : "+ kia.color);
+		System.out.println("최고 속도 : "+ kia.maxSpeed);
+		System.out.println("현재 속도 : "+ kia.speed);
+		
+		
+	}
+
+}
