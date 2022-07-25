604cfc41c3ee8450488028a8c7b1e544f6ed0f48
ondjj <jcjk0302@gmail.com>
2022-07-25 13:04:39 +0900

    quizquiz

--- src/morningJava/Quiz01.java
+++ src/morningJava/Quiz01.java
@@ -35,7 +35,7 @@ public class Quiz01 {
 			System.out.println(Arrays.toString(arr));
 		} while (flag);
 
-		System.out.println();
+		System.out.println("=================================");
 
 		System.out.print("내 풀이 : ");
 		arr[0] = (int) (Math.random() * 10) + 1;
@@ -44,12 +44,29 @@ public class Quiz01 {
 			int temp = (int) (Math.random() * 10) + 1;
 			if (arr[i - 1] != temp) {
 				arr[i] = temp;
+			}else {
+				i--;
 			}
 		}
 
 		for (int i : arr) {
 			System.out.print(i + " ");
 		}
+		
+		System.out.println();
+		System.out.println("=================================");
+		
+		System.out.print("강사님 풀이 : ");
+		arr[0] = (int) (Math.random() * 10) + 1;
+
+		for (int i = 1; i < arr.length; i++) {
+			do{
+				arr[i] = (int)(Math.random()*10)+1;
+			}while(arr[i]==arr[i-1]);
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		
 
 	}
 }
--- src/morningJava/Quiz02.java
+++ src/morningJava/Quiz02.java
@@ -1,9 +1,33 @@
 package morningJava;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Quiz02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 정수형 배열의 크기를 스캐너로 입력받은 정수만큼 크기로 생성하고 1~10사이의 랜덤값을 입력 받는다.
+		// 배열 요소가 동일한 값을 가지지 않도록 프로그램 해보자
+		// 예 --> {1,3,5,5,3,2} 5가 동일하게 나오면 x
+		
+		Scanner in = new Scanner(System.in);
+		int[] arr = new int[in.nextInt()];
+		System.out.print("내 풀이 : ");
+		
+		arr[0] = (int) (Math.random() * 10) + 1;
+
+		for (int i = 1; i < arr.length; i++) {
+			int j;
+			do {
+				arr[i] = (int)(Math.random()*10)+1;
+				for (j = 0; j < i; j++) {
+					if(arr[i]==arr[j]) break;
+					
+				}
+			}while(j<i);
+		
+		}
+		System.out.println(Arrays.toString(arr));
 
 	}
 
